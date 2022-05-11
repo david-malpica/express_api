7af5846023ae222a5cf61feb734278e8309bde36
David Malpica <dmalpica96@hotmail.com>
2022-05-11 03:03:03 -0500

    Add GET method

--- app.js
+++ app.js
@@ -11,3 +11,21 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
+// HTTP METHODS
+// GET method
+app.get('/v1/explorers', (req,res) => {
+    console.log(`API explorers GET all requests ${new Date()}`)
+    const explorer1 = {id: 1, name: "David1"}
+    const explorer2 = {id: 2, name: "David2"}
+    const explorer3 = {id: 3, name: "David3"}
+    const explorer4 = {id: 4, name: "David4"}
+    const explorers = [explorer1,explorer2,explorer3,explorer4]
+    res.status(200).json(explorers)
+})
+
+app.get('/v1/explorers/:id',(req,res) => {
+    console.log(`API explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id: 1,name: "David"}
+    res.status(200).json(explorer)
+})
